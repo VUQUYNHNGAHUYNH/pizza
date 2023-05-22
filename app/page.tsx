b58d0755f81358f3a4ac464ce21b326d2855a9d8
NGA <r0917268@student.thomasmore.be>
2023-05-22 13:38:31 +0700

    pizza component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import pizzas from "@/data";
+import Pizza from "./components/Pizza";
+
 export default function Home() {
-  return <div className="text-rose-600">Home</div>;
+  return (
+    <div className="container mx-auto">
+      {/* pizza grid */}
+      <div className=" grid gap-8 lg:gap-4 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+        {pizzas.map((pizza) => (
+          <Pizza pizza={pizza} key={pizza.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
